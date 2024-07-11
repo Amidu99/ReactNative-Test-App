c52c5f2e2516b213e872dbfa121d234c18c4e69a
Amidu99 <amidushamika@gmail.com>
2024-07-11 11:43:01 +0530

    check FlatList

--- app/(tabs)/details.tsx
+++ app/(tabs)/details.tsx
@@ -1,10 +1,38 @@
-import { Button, View, Text, StyleSheet } from 'react-native';
 import { router } from 'expo-router';
+import {
+  Button,
+  SafeAreaView,
+  View,
+  FlatList,
+  StyleSheet,
+  Text,
+  StatusBar,
+} from 'react-native';
+
+const DATA = [
+  {
+    id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
+    title: 'First Item',
+  },
+  {
+    id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
+    title: 'Second Item',
+  },
+  {
+    id: '58694a0f-3da1-471f-bd96-145571e29d72',
+    title: 'Third Item',
+  },
+];
 
 export default function DetailsScreen() {
   return (
     <View style={styles.container}>
       <Text>Details</Text>
+      <FlatList
+        data={DATA}
+        renderItem={({item}) => <Text style={styles.item}>{item.title}</Text>}
+        keyExtractor={item => item.id}
+      />
       <Button
         title="Back to Home"
         color="#f194ff"
@@ -19,5 +47,11 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+  },
+  item: {
+    backgroundColor: '#f9c2ff',
+    padding: 20,
+    marginVertical: 8,
+    marginHorizontal: 16,
   },
 });
