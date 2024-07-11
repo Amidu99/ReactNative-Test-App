86d9e40dfa6d679894c24104f17d682361b00130
Amidu99 <amidushamika@gmail.com>
2024-07-11 11:02:51 +0530

    add back btn

--- app/details.tsx
+++ app/details.tsx
@@ -1,9 +1,15 @@
-import { View, Text, StyleSheet } from 'react-native';
+import { Button, View, Text, StyleSheet } from 'react-native';
+import { router } from 'expo-router';
 
 export default function DetailsScreen() {
   return (
     <View style={styles.container}>
       <Text>Details</Text>
+      <Button
+        title="Back to Home"
+        color="#f194ff"
+        onPress={() => router.back()}
+      />
     </View>
   );
 }
