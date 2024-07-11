a2e8c30b31786020e9e2a6b70959ba9cb2cb4c68
Amidu99 <amidushamika@gmail.com>
2024-07-11 10:39:38 +0530

    style header

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,9 +2,20 @@ import { Stack } from "expo-router";
 
 export default function RootLayout() {
   return (
-    <Stack>
-      <Stack.Screen name="index" />
-      <Stack.Screen name="details" />
+    /* styles for main header */
+    <Stack
+      screenOptions={{
+        headerStyle: {
+          backgroundColor: '#f4511e',
+        },
+        headerTintColor: '#fff',
+        headerTitleStyle: {
+          fontWeight: 'bold',
+        },
+      }}>
+      {/* custom styles for one header */}
+      <Stack.Screen name="index" options={{headerTitle:'Home', headerShown:false}}/>
+      <Stack.Screen name="details" options={{headerStyle: {backgroundColor: 'blue'}}}/>
     </Stack>
   );
 }
